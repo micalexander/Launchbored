65fe06e6ba0c7e80623a382ef325a9bb0bf96645
Mic Alexander <micalexander@Mics-MacBook-Pro.local>
2012-12-05 13:28:01 -0700

    created new file structure and made css edits

--- view/footer.php
+++
@@ -1,4 +0,0 @@
-<script src="js/modernizr-transitions.js"></script>
-<script src="js/jquery-1.7.1.min.js"></script>
-<script src="jquery.isotope.min.js"></script>
-<script src="js/launchbored.js"></script>
